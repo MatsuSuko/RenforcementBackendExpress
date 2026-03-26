34c3d70d9b732c26cb4a1e52430e9ca7ee806099
Souvanny <souvannyb21@gmail.com>
2026-03-26 15:02:44 +0100

    ajout du hashage mdp

--- services/users.js
+++ services/users.js
@@ -1,124 +1,111 @@
 const { Op } = require('sequelize');
-const { User, dbInstance } = require('../models')
+const { User, dbInstance } = require('../models');
+const bcrypt = require('bcrypt');
+require('dotenv').config();
 
 const getAllUsers = async (req, res) => {
     let queryParam = {};
-    if(req.query?.search) {
+    if (req.query?.search) {
         queryParam = {
             where: {
-                firstname : {
+                firstname: {
                     [Op.like]: `%${req.query.search}%`
                 }
             }
-        }
+        };
     }
     const users = await User.findAll(queryParam);
-    res.status(200).json({
-        users
-    })
-}
+    res.status(200).json({ users });
+};
 
 const getUser = async (req, res) => {
     const id = req.params.id;
-    // const user = await User.findByPk(id);
-    const user = await User.findOne({
-        where: { id }
-    })
-    res.status(200).json({
-        user
-    })
-}
+    const user = await User.findOne({ where: { id } });
+    res.status(200).json({ user });
+};
 
 const createUser = async (req, res) => {
     const transaction = await dbInstance.transaction();
     try {
-        const { username, firstname, lastname, email, password } = req.body
+        const { username, firstname, lastname, email, password, role } = req.body;
+        const hashedpassword = await bcrypt.hash(password, parseInt(process.env.BCRYPT_SALT));
+
         const user = await User.create({
             username,
             firstname,
             lastname,
             email,
-            password
-        }, { transaction })
+            password: hashedpassword,
+            role: role || 'insured'
+        }, { transaction });
 
-        transaction.commit();
+        await transaction.commit();
         return res.status(201).json({
-            user
-        })
-    } catch(err) {
-        transaction.rollback();
+            user: user.clean()
+        });
+    } catch (err) {
+        await transaction.rollback();
         return res.status(400).json({
             message: 'Error on user creation',
             stacktrace: err.errors
-        })
+        });
     }
-}
+};
 
 const updateUser = async (req, res) => {
     const transaction = await dbInstance.transaction();
     try {
-        const { username, firstname, lastname, email, password } = req.body
-        const user_id = req.params.id
-        // meilleur manière de mettre à jour :
-        const user = await User.update({
+        const { username, firstname, lastname, email, role, active } = req.body;
+        const user_id = req.params.id;
+
+        await User.update({
             username,
             firstname,
             lastname,
             email,
-            password
+            role,
+            active
         }, {
             where: { id: user_id },
             transaction
-        })
+        });
 
-        // autre option :
-        // const user = await User.findOne({
-        //     where: { id: user_id }
-        // }, { transaction })
-        // user.firstname = firstname
-        // user.username = username
-        // user.lastname = lastname
-        // user.email = email
-        // user.password = password
-        // await user.save()
-
-        transaction.commit();
+        await transaction.commit();
         return res.status(200).json({
-            message: "Successfuly updated",
-            user
-        })
-    } catch(err) {
-        transaction.rollback();
+            message: 'Successfuly updated'
+        });
+    } catch (err) {
+        await transaction.rollback();
         return res.status(400).json({
             message: 'Error on user update',
             stacktrace: err.errors
-        })
+        });
     }
-}
+};
 
 const deleteUser = async (req, res) => {
     const transaction = await dbInstance.transaction();
     try {
-        const user_id = req.params.id
-        
+        const user_id = req.params.id;
+
         const status = await User.destroy({
             where: { id: user_id },
             transaction
-        })
+        });
 
-        transaction.commit();
+        await transaction.commit();
         return res.status(200).json({
-            message: "Successfuly deleted",
+            message: 'Successfuly deleted',
             status
-        })
-    } catch(err) {
-        transaction.rollback();
+        });
+    } catch (err) {
+        await transaction.rollback();
         return res.status(400).json({
             message: 'Error on user deletion',
             stacktrace: err.errors
-        })
+        });
     }
-}
+};
 
 module.exports = {
     getAllUsers,
@@ -126,4 +113,4 @@ module.exports = {
     createUser,
     updateUser,
     deleteUser
-}
+};
